cc1906e0f7b2b57e621726fa6178d94d7f7c9e6a
arpit <arpit2003y@gmail.com>
2024-10-07 17:08:02 +0530

    fix(loader):added_missing_loading_state

--- src/app/api/accept-messages/route.ts
+++ src/app/api/accept-messages/route.ts
@@ -4,108 +4,108 @@ import { ConnectToDatabase } from "@/db/dbConnection"
 import UserModel, { User } from "@/models/user"
 
 export async function POST(req: Request) {
-    await ConnectToDatabase()
+	await ConnectToDatabase()
 
-    const session = await getServerSession(authOptions)
-    const user = session?.user as User
+	const session = await getServerSession(authOptions)
+	const user = session?.user as User
 
-    if (!session || !user) {
-        return Response.json(
-            {
-                message: "You must be logged in to use this feature",
-                success: false,
-            },
-            { status: 401 }
-        )
-    }
+	if (!session || !user) {
+		return Response.json(
+			{
+				message: "You must be logged in to use this feature",
+				success: false,
+			},
+			{ status: 401 }
+		)
+	}
 
-    const userId = user._id
-    const { acceptMessage } = await req.json()
+	const userId = user._id
+	const { acceptMessage } = await req.json()
 
-    try {
-        const updatedUser = await UserModel.findByIdAndUpdate(
-            userId,
-            {
-                isAcceptingMessages: acceptMessage,
-            },
-            { new: true }
-        )
+	try {
+		const updatedUser = await UserModel.findByIdAndUpdate(
+			userId,
+			{
+				isAcceptingMessages: acceptMessage,
+			},
+			{ new: true }
+		)
 
-        if (!updatedUser) {
-            return Response.json(
-                {
-                    message:
-                        "Unable to find the user to update the acceptance status",
-                    success: false,
-                },
-                { status: 404 }
-            )
-        }
+		if (!updatedUser) {
+			return Response.json(
+				{
+					message:
+						"Unable to find the user to update the acceptance status",
+					success: false,
+				},
+				{ status: 404 }
+			)
+		}
 
-        return Response.json(
-            {
-                success: true,
-                message: "User updated successfully",
-                isAcceptingMessages: updatedUser.isAcceptingMessages,
-            },
-            { status: 200 }
-        )
-    } catch (error) {
-        console.error("Error updating user acceptance status", error)
-        return Response.json(
-            {
-                success: false,
-                message: "Error updating user acceptance status",
-            },
-            { status: 500 }
-        )
-    }
+		return Response.json(
+			{
+				success: true,
+				message: "User updated successfully",
+				isAcceptingMessages: updatedUser.isAcceptingMessages,
+			},
+			{ status: 200 }
+		)
+	} catch (error) {
+		console.error("Error updating user acceptance status", error)
+		return Response.json(
+			{
+				success: false,
+				message: "Error updating user acceptance status",
+			},
+			{ status: 500 }
+		)
+	}
 }
 
 export async function GET() {
-    await ConnectToDatabase()
+	await ConnectToDatabase()
 
-    const session = await getServerSession(authOptions)
-    const user = session?.user as User
+	const session = await getServerSession(authOptions)
+	const user = session?.user as User
 
-    if (!session || !user) {
-        return Response.json(
-            {
-                message: "You must be logged in to use this feature",
-                success: false,
-            },
-            { status: 401 }
-        )
-    }
+	if (!session || !user) {
+		return Response.json(
+			{
+				message: "You must be logged in to use this feature",
+				success: false,
+			},
+			{ status: 401 }
+		)
+	}
 
-    try {
-        const foundUser = await UserModel.findById(user._id)
+	try {
+		const foundUser = await UserModel.findById(user._id)
 
-        if (!foundUser) {
-            return Response.json(
-                {
-                    message: " User not found",
-                    success: false,
-                },
-                { status: 404 }
-            )
-        }
+		if (!foundUser) {
+			return Response.json(
+				{
+					message: " User not found",
+					success: false,
+				},
+				{ status: 404 }
+			)
+		}
 
-        return Response.json(
-            {
-                success: true,
-                isAcceptingMessages: foundUser.isAcceptingMessages,
-            },
-            { status: 200 }
-        )
-    } catch (error) {
-        console.error("Error getting user acceptance status", error)
-        return Response.json(
-            {
-                success: false,
-                message: "Error getting user acceptance status",
-            },
-            { status: 500 }
-        )
-    }
+		return Response.json(
+			{
+				success: true,
+				isAcceptingMessages: foundUser.isAcceptingMessages,
+			},
+			{ status: 200 }
+		)
+	} catch (error) {
+		console.error("Error getting user acceptance status", error)
+		return Response.json(
+			{
+				success: false,
+				message: "Error getting user acceptance status",
+			},
+			{ status: 500 }
+		)
+	}
 }
--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -3,11 +3,11 @@ import { Button } from "@/components/ui/button"
 import { z } from "zod"
 import { Input } from "@/components/ui/input"
 import {
-    Form,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
+	Form,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
 } from "@/components/ui/form"
 import { signIn } from "next-auth/react"
 import Link from "next/link"
@@ -16,94 +16,110 @@ import { useForm } from "react-hook-form"
 import { signInSchema } from "@/validation/signInSchema"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useToast } from "@/components/ui/use-toast"
+import { useState } from "react"
+import { Loader2 } from "lucide-react"
 
 export default function LoginForm() {
-    const { toast } = useToast()
-    const router = useRouter()
+	const { toast } = useToast()
+	const router = useRouter()
+	const [isLoading, setIsLoading] = useState<boolean>(false)
 
-    const form = useForm<z.infer<typeof signInSchema>>({
-        resolver: zodResolver(signInSchema),
-        defaultValues: {
-            email: "",
-            password: "",
-        },
-    })
+	const form = useForm<z.infer<typeof signInSchema>>({
+		resolver: zodResolver(signInSchema),
+		defaultValues: {
+			email: "",
+			password: "",
+		},
+	})
 
-    const onSubmit = async (data: z.infer<typeof signInSchema>) => {
-        try {
-            const result = await signIn("credentials", {
-                redirect: false,
-                email: data.email,
-                password: data.password,
-            })
+	const onSubmit = async (data: z.infer<typeof signInSchema>) => {
+		try {
+			setIsLoading(true)
+			const result = await signIn("credentials", {
+				redirect: false,
+				email: data.email,
+				password: data.password,
+			})
 
-            if (result?.status === 200) {
-                router.replace("/admin/dashboard")
-            }
-        } catch (error: any) {
-            toast({
-                title: "Error",
-                description: error.message || "Unkown error occured",
-                variant: "destructive",
-            })
-        }
-    }
+			if (result?.status === 200) {
+				router.replace("/admin/dashboard")
+			}
+		} catch (error: any) {
+			toast({
+				title: "Error",
+				description: error.message || "Unkown error occured",
+				variant: "destructive",
+			})
+		} finally {
+			setIsLoading(false)
+		}
+	}
 
-    return (
-        <div className="flex justify-center items-center min-h-screen ">
-            <div className="w-full max-w-md p-8 space-y-8  ">
-                <div className="text-center">
-                    <h1 className="text-xl font-extrabold tracking-tight lg:text-xl  mb-6">
-                        <span className="text-6xl text-[#d5b9b2] mr-2">
-                            Welcome
-                        </span>{" "}
-                        Back !
-                    </h1>
-                    <p className="mb-4 text-sm text-gray-300">
-                        Sign in to continue your secret conversations
-                    </p>
-                </div>
-                <Form {...form}>
-                    <form
-                        onSubmit={form.handleSubmit(onSubmit)}
-                        className="space-y-6"
-                    >
-                        <FormField
-                            name="email"
-                            control={form.control}
-                            render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel>Email</FormLabel>
-                                    <Input type="email" {...field} />
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                        />
-                        <FormField
-                            name="password"
-                            control={form.control}
-                            render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel>Password</FormLabel>
-                                    <Input type="password" {...field} />
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                        />
-                        <Button className="w-full" type="submit">
-                            Sign In
-                        </Button>
-                    </form>
-                </Form>
-                <div className="text-center mt-4">
-                    <p>
-                        Not a member yet?{" "}
-                        <Link href="/auth/sign-up" className="text-[#d5b9b2]">
-                            Sign up
-                        </Link>
-                    </p>
-                </div>
-            </div>
-        </div>
-    )
+	return (
+		<div className="flex justify-center items-center min-h-screen ">
+			<div className="w-full max-w-md p-8 space-y-8  ">
+				<div className="text-center">
+					<h1 className="text-xl font-extrabold tracking-tight lg:text-xl  mb-6">
+						<span className="text-6xl text-[#d5b9b2] mr-2">
+							Welcome
+						</span>{" "}
+						Back !
+					</h1>
+					<p className="mb-4 text-sm text-gray-300">
+						Sign in to continue your secret conversations
+					</p>
+				</div>
+				<Form {...form}>
+					<form
+						onSubmit={form.handleSubmit(onSubmit)}
+						className="space-y-6"
+					>
+						<FormField
+							name="email"
+							control={form.control}
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Email</FormLabel>
+									<Input type="email" {...field} />
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+						<FormField
+							name="password"
+							control={form.control}
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Password</FormLabel>
+									<Input type="password" {...field} />
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+						<Button className="w-full" type="submit" disabled={isLoading}>
+							{
+
+								isLoading ? (
+									<div className="flex gap-1">
+										<Loader2 className="mr-2 h-4 w-4 animate-spin" />
+										<span>
+											"signing in..."
+										</span>
+									</div>
+								) : "Sign in"
+							}
+						</Button>
+					</form>
+				</Form>
+				<div className="text-center mt-4">
+					<p>
+						Not a member yet?{" "}
+						<Link href="/auth/sign-up" className="text-[#d5b9b2]">
+							Sign up
+						</Link>
+					</p>
+				</div>
+			</div>
+		</div>
+	)
 }
